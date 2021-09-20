099fa3850bddd4c6bec61ab0d086dfccca270f7c
jang13579 <80467890+jang13579@users.noreply.github.com>
2021-09-21 00:51:58 +0900

    욱제는 도박쟁이

---
+++ src/Greedy_14655.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Greedy_14655 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n, sum=0;
+
+        n = scanner.nextInt();
+        int[] v = new int[n];
+
+        for (int i = 0; i < n; i++){
+            v[i] = Math.abs(scanner.nextInt());
+            sum += v[i];
+        }
+        sum = sum * 2;
+        System.out.println(sum);
+    }
+}
